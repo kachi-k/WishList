f22f9e358880180b1c86e60d633bd822f9092fce
kachi-k <olamilarex@gmail.com>
2020-08-20 02:53:54 +0200

    First commit(add and edit wish)

---
+++ app/src/main/java/com/kachi/wishlist/AddNewWish.kt
@@ -0,0 +1,11 @@
+package com.kachi.wishlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNewWish : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_wish)
+    }
+}
---
+++ app/src/main/java/com/kachi/wishlist/EditWishList.kt
@@ -0,0 +1,11 @@
+package com.kachi.wishlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditWishList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_wish_list)
+    }
+}
